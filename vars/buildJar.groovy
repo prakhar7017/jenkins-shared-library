80b38bfade1c561e0fc0139bfc0656deefb89052
Prakhar Maheshwari <prakharmaheshwari96@gmail.com>
2026-02-01 00:58:29 +0530

    update config

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,4 +1,6 @@
 #!/usr/bin/env groovy
+import com.example.Docker
+
 def call() {
     echo "building the application $BRANCH_NAME"
     sh 'mvn package'
--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -1,4 +1,5 @@
 #!/usr/bin/env groovy
+import com.example.Docker
 
 def call(String imageName) {
     return new Docker(this).dockerLogin(imageName)
